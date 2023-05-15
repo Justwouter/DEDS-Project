cb017f2fad07cd48c6a37fd3202bb0f1dc1246fa
Justwouter <wouter2312@gmail.com>
2023-05-15 09:42:17 +0200

    Added some debug info

--- Funda.py
+++ Funda.py
@@ -22,6 +22,8 @@ adLinks = []
 listings = []
 wait = ""
 
+#TODO 
+#Actually implement project dir saving without hardcoded paths
         
 def getBrowser():
     webdriver.Chrome.get
@@ -30,7 +32,7 @@ def getBrowser():
     options = webdriver.ChromeOptions()
     # options.add_argument("--log-level=3")
     # options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("enable-automation")
     options.add_argument("--no-sandbox")
@@ -148,8 +150,11 @@ class MyThread(Thread):
             adURL = getItemFromLock(dataLock,adLinks)
             try:
                 listingInfo = FundaGetListingInfo(browser, adURL)
+                print(len(adLinks))
             except Exception as e:
+                # Print some debug info and restart the browser.
                 print(e)
+                print(browser.current_url)
                 browser.close()
                 browser = getBrowser()
                 adLinks.append(adURL)
